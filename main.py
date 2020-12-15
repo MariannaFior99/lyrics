d8299f3d5432b598dd34b46f79b7f4225ad8709c
Marianna Fior <mariannafior@mbp-di-marianna.homenet.telecomitalia.it>
2020-12-15 15:29:48 +0100

    parse_arguments

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-# import file
+# import file and package
 from scripts import lyrics_extraction
+import argparse
+
+
+def parse_arguments():
+
+    '''Insert artist and song inputs.'''
+
+    parser = argparse.ArgumentParser(description='Show Lyrics')
+    # Selection of the artsit
+    parser.add_argument('artist', type=str, help='Insert an artist',
+                        default=None)
+    # Selection of the song
+    parser.add_argument('song', type=str, help='Insert a song', default=None)
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-q', '--quiet', action='store_true',
+                       help='print quiet')
+    # Selection of the level of verbosity
+    group.add_argument('-v', '--verbose', action='store_true',
+                       help='print verbose')
+    args = parser.parse_args()
+
+    return args
+
 
 if __name__ == '__main__':
+    # run parse argument
+    args = parse_arguments()
     # run function from lyrics_extraction file
-    data = lyrics_extraction.get_lyrics("Rihanna", "Diamonds")
+    data = lyrics_extraction.get_lyrics(args.artist, args.song)
